14fa68c64705252f25f7cbcec602d09a89f14ec4
Ralph Shui <rskc.2112@gmail.com>
2023-07-25 19:15:51 -0400

    completed lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,18 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    for name in names:
+        print(f"{names.index(name)+1}. {name}")
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+   return [planetneer.capitalize() + "!" for planetneer in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(call_list):
+    for call in call_list:
+        if (len(call) > 4):
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(list):
+    cheeses = ["cheddar","gouda","camembert"]
+    for item in list:
+        if (item in cheeses):
+            return item
